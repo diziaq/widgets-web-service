fda4961b3eddf8c3b05fc8ea81b21ba8d5b6e26c
diziaq <diziaq@nowhere.org>
2020-10-31 15:46:58 +0500

    Configured logging

--- src/test/java/sn/sandbox/mirowidgets/ApplicationCanStartTest.java
+++ src/test/java/sn/sandbox/mirowidgets/ApplicationCanStartTest.java
@@ -1,14 +1,18 @@
 package sn.sandbox.mirowidgets;
 
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 
 
 @SpringBootTest
@@ -27,5 +31,15 @@ class ApplicationCanStartTest {
   @DisplayName("when app is started then ApplicationMain bean exists in context")
   void contextContainsMainBean() {
     assertNotNull(applicationContext.getBean(ApplicationMain.class));
+  }
+
+
+  @Test
+  @DisplayName("when app is started with defaults then environment has active profiles [basic,logging]")
+  void environmentContainsSpecificProfiles() {
+    var environment = applicationContext.getBean(Environment.class);
+    var activeProfiles = List.of(environment.getActiveProfiles());
+
+    assertThat(activeProfiles, contains("basic", "logging"));
   }
 }
